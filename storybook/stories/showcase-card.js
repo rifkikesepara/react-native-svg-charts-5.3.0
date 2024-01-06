43126bc0a51520440616c8d872a7c0bb3a135025
rifkikesepara <rifkikesepara7@gmail.com>
2024-01-06 13:34:19 +0300

    Init

---
+++ storybook/stories/showcase-card.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { SafeAreaView, View } from 'react-native'
+
+const ShowcaseCard = ({ children }) => (
+    <SafeAreaView>
+        <View
+            style={{
+                margin: 16,
+                paddingHorizontal: 16,
+                backgroundColor: 'white',
+                shadowOffset: {
+                    height: 2,
+                    width: 2,
+                },
+                elevation: 4,
+                shadowColor: 'black',
+                shadowOpacity: 0.5,
+            }}
+        >
+            {children}
+        </View>
+    </SafeAreaView>
+)
+
+export default ShowcaseCard
